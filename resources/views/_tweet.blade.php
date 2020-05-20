0cd0e3f81fd63af9a361ae8dda26b134128485c3
SharkdanDaniel <sharkdan_daniel@hotmail.com>
2020-05-20 18:31:03 -0300

    Added home page, tweets, friends list, avatars, menu bars

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,16 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-4">
+        <img
+            src="https://i.pravatar.cc/50"
+            alt=""
+            class="rounded-full mr-2"
+        >
+    </div>
+
+    <div>
+        <h5 class="font-bold mb-2 flex-shrink-0">John Doe</h5>
+        <p class="text-sm">
+            Look, I'm looking forward to seeing you again. Let's talk more later, givime a call when you're free.
+        </p>
+    </div>
+</div>
