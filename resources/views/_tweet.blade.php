0416912a9220409642ef91558c3afd87308f029a
SharkdanDaniel <sharkdan_daniel@hotmail.com>
2020-05-21 18:59:56 -0300

    Created follows system and add a tweet

--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -1,16 +1,17 @@
 <div class="flex p-4 border-b border-b-gray-400">
     <div class="mr-4">
         <img
-            src="https://i.pravatar.cc/50"
+            src="{{ $tweet->user->avatar }}"
             alt=""
             class="rounded-full mr-2"
         >
     </div>
 
     <div>
-        <h5 class="font-bold mb-2 flex-shrink-0">John Doe</h5>
+        <h5 class="font-bold mb-2 flex-shrink-0">{{ $tweet->user->name }}</h5>
+
         <p class="text-sm">
-            Look, I'm looking forward to seeing you again. Let's talk more later, givime a call when you're free.
+            {{ $tweet->body }}
         </p>
     </div>
 </div>
